280756ab84af5e4821955152fbcf413a58560535
Alexdelia <alexandre.delille.57@gmail.com>
2021-11-23 14:56:39 +0100

    remove ft_ps

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -6,19 +6,13 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 14:07:40 by adelille          #+#    #+#             */
-/*   Updated: 2021/11/23 14:51:41 by adelille         ###   ########.fr       */
+/*   Updated: 2021/11/23 14:56:11 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-#define DEFAULT	"* LOUD AND UNBEARABLE FEEDBACK NOISE *\n"
-
-int	ft_ps(char *str)
-{
-	std::cout << str;
-	return (0);
-}
+#define DEFAULT	"* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 void	ft_toupper(char *str)
 {
@@ -38,12 +32,15 @@ int	main(int ac, char **av)
 	int	i;
 
 	if (ac == 1)
-		return (ft_ps((char *)DEFAULT));
+	{
+		std::cout << DEFAULT << std::endl;
+		return (0);
+	}
 	i = 1;
 	while (i < ac)
 	{
 		ft_toupper(av[i]);
-		ft_ps(av[i]);
+		std::cout << av[i];
 		i++;
 	}
 	std::cout << std::endl;
