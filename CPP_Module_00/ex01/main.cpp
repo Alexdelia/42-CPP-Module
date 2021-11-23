c18255bf6dbe5f5cd0b4917f9cab01768cd42028
Alexdelia <alexandre.delille.57@gmail.com>
2021-11-23 15:21:22 +0100

    starting ex01

---
+++ CPP_Module_00/ex01/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 14:30:46 by adelille          #+#    #+#             */
+/*   Updated: 2021/11/23 15:21:02 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int ac, char **av)
+{
+	// EXIT -> exit
+	// ADD -> call user to input:
+	//		first name, last name, nickname, phone number, darkest secret.
+	
+	return (0);
+}
