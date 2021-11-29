b6895dfcdc61c2d03cd8a3bdc92c2a20c2ac2fef
adelille <adelille@student.42.fr>
2021-11-29 13:33:22 +0100

    fix makefil exec name, test to open file

--- CPP_Module_01/ex04/main.cpp
+++ CPP_Module_01/ex04/main.cpp
@@ -6,20 +6,32 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:31:13 by adelille          #+#    #+#             */
-/*   Updated: 2021/11/27 16:38:18 by adelille         ###   ########.fr       */
+/*   Updated: 2021/11/29 13:32:43 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+
 #include "../color.hpp"
 
 int	main(int ac, char **av)
 {
+	std::string		search;
+	std::string		replace;
+	
 	(void)av;
 	if (ac != 4)
 	{
 		std::cout << C_RED << C_BOLD << "Error: Wrong number of arguments" << std::endl << C_RESET
-			<< "Usage: ./Sed_is_for_losers [file] [string to search] [string to replace]" << std::endl;
+			<< "Usage: ./replace [file] [string to search] [string to replace]" << std::endl;
 		return (1);
 	}
+	std::ifstream	ifs(av[1]);
+	if (!ifs.is_open())
+	{
+		std::cout << C_RED << C_BOLD << "Error: " << av[1] << " failed to open" << std::endl;
+		return (2);
+	}
+	return (0);
 }
