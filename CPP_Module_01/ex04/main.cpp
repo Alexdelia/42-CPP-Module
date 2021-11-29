fdf7bbbca7ce3203e13e468dd9da883a1ab3773e
adelille <adelille@student.42.fr>
2021-11-29 13:40:33 +0100

    testing and continueing

--- CPP_Module_01/ex04/main.cpp
+++ CPP_Module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:31:13 by adelille          #+#    #+#             */
-/*   Updated: 2021/11/29 13:32:43 by adelille         ###   ########.fr       */
+/*   Updated: 2021/11/29 13:40:05 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 
 int	main(int ac, char **av)
 {
-	std::string		search;
-	std::string		replace;
+	std::string	search;
+	std::string	replace;
+	std::string	output;
+	std::string	file;
 	
-	(void)av;
 	if (ac != 4)
 	{
 		std::cout << C_RED << C_BOLD << "Error: Wrong number of arguments" << std::endl << C_RESET
@@ -33,5 +34,12 @@ int	main(int ac, char **av)
 		std::cout << C_RED << C_BOLD << "Error: " << av[1] << " failed to open" << std::endl;
 		return (2);
 	}
+	search = av[2];
+	replace = av[3];
+	if (search.length() < 1 || replace.length() < 1)
+		std::cout << C_YELLOW << "Warning: Input shouldn't be an empty string" << std::endl;
+	file = (std::string)av[1] + (std::string)".replace";
+	//std::cout << file << std::endl;
+	//while (getline(ifs, 
 	return (0);
 }
