8627a69227f2f593d804c525ef40842c1b99fd92
adelille <adelille@student.42.fr>
2021-11-30 09:28:49 +0100

    very slowly going on and testing stuff, certainly far from final result

--- CPP_Module_01/ex04/main.cpp
+++ CPP_Module_01/ex04/main.cpp
@@ -6,21 +6,60 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 16:31:13 by adelille          #+#    #+#             */
-/*   Updated: 2021/11/29 13:40:05 by adelille         ###   ########.fr       */
+/*   Updated: 2021/11/30 09:27:32 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+//#include <string>
 
 #include "../color.hpp"
 
+std::string	sed(std::string input, std::string search, std::string replace)
+{
+	std::string	output;
+	int			i;
+	int			x;
+	int			y;
+	int			o;
+
+	i = 0;
+	o = 0;
+	while (input[i])
+	{
+		x = i;
+		y = 0;
+		while (search[y] && input[x] && search[y] == input[x])
+		{
+			x++;
+			y++;
+		}
+		if (!search[y])
+		{
+			i = x;
+			y = 0;
+			while (replace[y])
+			{
+				output[o] = replace[y];
+				o++;
+				y++;
+			}
+		}
+		else
+			output[o] = input[i];
+		i++;
+	}
+	return (output);
+}
+
 int	main(int ac, char **av)
 {
+	std::string	file;
 	std::string	search;
 	std::string	replace;
+	std::string	tmp;
 	std::string	output;
-	std::string	file;
 	
 	if (ac != 4)
 	{
@@ -39,7 +78,14 @@ int	main(int ac, char **av)
 	if (search.length() < 1 || replace.length() < 1)
 		std::cout << C_YELLOW << "Warning: Input shouldn't be an empty string" << std::endl;
 	file = (std::string)av[1] + (std::string)".replace";
-	//std::cout << file << std::endl;
-	//while (getline(ifs, 
+	while (getline(ifs, tmp))
+	{
+		output += tmp;
+		if (!ifs.eof())
+			output += "\n";
+	}
+	output = sed(output, search, replace);
+	std::ofstream	ofs(file.c_str());
+	ofs << output;
 	return (0);
 }
