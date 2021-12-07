3e99ab9a12c92cbbbbe07867efc67143eaf0a1fa
Alexdelia <alexandre.delille.57@gmail.com>
2021-12-07 14:17:45 +0100

    ex02

--- CPP_Module_02/ex02/Fixed.cpp
+++ CPP_Module_02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:28:37 by adelille          #+#    #+#             */
-/*   Updated: 2021/12/07 12:41:52 by adelille         ###   ########.fr       */
+/*   Updated: 2021/12/07 14:17:27 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,130 @@ const int	Fixed::_bits = 8;
 
 Fixed::Fixed(void) : _fix(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+	//std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(Fixed const & src)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	//std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
 Fixed::Fixed(const int i) : _fix(0)
 {
-	std::cout << "Int constructor called" << std::endl;
+	//std::cout << "Int constructor called" << std::endl;
 	this->_fix = i << this->_bits;
 }
 
 Fixed::Fixed(const float f) : _fix(0)
 {
-	std::cout << "Float constructor called" << std::endl;
+	//std::cout << "Float constructor called" << std::endl;
 	this->_fix = int(roundf(f * (1 << this->_bits)));
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	//std::cout << "Destructor called" << std::endl;
 }
 
 Fixed	&Fixed::operator=(Fixed const & rhs)
 {
-	std::cout << "Assignment operator called" << std::endl;
+	//std::cout << "Assignment operator called" << std::endl;
 
 	if (this != &rhs)
 		this->_fix = rhs.getRawBits();
 	
 	return (*this);
+}
+
+bool	Fixed::operator==(const Fixed &rhs) const
+{
+	return (this->_fix == rhs.getRawBits());
+}
+
+bool	Fixed::operator!=(const Fixed &rhs) const
+{
+	return (this->_fix != rhs.getRawBits());
+}
+
+bool	Fixed::operator<(const Fixed &rhs) const
+{
+	return (this->_fix < rhs.getRawBits());
+}
+
+bool	Fixed::operator>(const Fixed &rhs) const
+{
+	return (this->_fix > rhs.getRawBits());
+}
+
+bool	Fixed::operator<=(const Fixed &rhs) const
+{
+	return (this->_fix <= rhs.getRawBits());
+}
+
+bool	Fixed::operator>=(const Fixed &rhs) const
+{
+	return (this->_fix >= rhs.getRawBits());
+}
+
+Fixed	Fixed::operator+(const Fixed &rhs) const
+{
+	Fixed	ret;
+
+	ret.setRawBits(this->_fix + rhs.getRawBits());
+	return (ret);
+}
+
+Fixed	Fixed::operator-(const Fixed &rhs) const
+{
+	Fixed	ret;
+
+	ret.setRawBits(this->_fix - rhs.getRawBits());
+	return (ret);
+}
+
+Fixed	Fixed::operator*(const Fixed &rhs) const
+{
+	Fixed	ret;
+
+	ret.setRawBits((this->_fix * rhs.getRawBits()) >> this->_bits);
+	return (ret);
+}
+
+Fixed	Fixed::operator/(const Fixed &rhs) const
+{
+	Fixed	ret;
+
+	ret.setRawBits((this->_fix << this->_bits)/ rhs.getRawBits());
+	return (ret);
+}
+
+Fixed	&Fixed::operator++(void)
+{
+	this->_fix++;
+	return (*this);
+}
+
+Fixed	&Fixed::operator--(void)
+{
+	this->_fix--;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed	ret(*this);
+
+	this->_fix++;
+	return (ret);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed	ret(*this);
+
+	this->_fix--;
+	return (ret);
 }
 
 int	Fixed::getRawBits(void) const
@@ -78,4 +168,24 @@ float	Fixed::toFloat(void) const
 int	Fixed::toInt(void) const
 {
 	return (this->_fix >> this->_bits);
+}
+
+Fixed	&Fixed::max(Fixed &lhs, Fixed &rhs)
+{
+	return (lhs > rhs ? lhs : rhs);
+}
+
+const Fixed	&Fixed::max(Fixed const &lhs, Fixed const &rhs)
+{
+	return (lhs > rhs ? lhs : rhs);
+}
+
+Fixed	&Fixed::min(Fixed &lhs, Fixed &rhs)
+{
+	return (lhs < rhs ? lhs : rhs);
+}
+
+const Fixed	&Fixed::min(Fixed const &lhs, Fixed const &rhs)
+{
+	return (lhs < rhs ? lhs : rhs);
 }
--- CPP_Module_02/ex02/Fixed.hpp
+++ CPP_Module_02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 14:26:23 by adelille          #+#    #+#             */
-/*   Updated: 2021/12/07 12:39:29 by adelille         ###   ########.fr       */
+/*   Updated: 2021/12/07 13:56:35 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,32 @@ class Fixed
 		~Fixed(void);
 
 		Fixed	&operator=(const Fixed &rhs);
+		Fixed	operator+(const Fixed &rhs) const;
+		Fixed	operator-(const Fixed &rhs) const;
+		Fixed	operator*(const Fixed &rhs) const;
+		Fixed	operator/(const Fixed &rhs) const;
+		Fixed	&operator++(void);
+		Fixed	&operator--(void);
+		Fixed	operator++(int);
+		Fixed	operator--(int);
+
+		bool	operator>(const Fixed &rhs) const;
+		bool	operator<(const Fixed &rhs) const;
+		bool	operator>=(const Fixed &rhs) const;
+		bool	operator<=(const Fixed &rhs) const;
+		bool	operator==(const Fixed &rhs) const;
+		bool	operator!=(const Fixed &rhs) const;
 
 		int		getRawBits(void) const;
 		void	setRawBits(const int raw);
 
 		float	toFloat(void) const;
 		int		toInt(void) const;
+
+		static Fixed		&min(Fixed &lhs, Fixed &rhs);
+		static const Fixed	&min(const Fixed &lhs, const Fixed &rhs);
+		static Fixed		&max(Fixed &lhs, Fixed &rhs);
+		static const Fixed	&max(const Fixed &lhs, const Fixed &rhs);
 
 	private:
 		static const int	_bits;
