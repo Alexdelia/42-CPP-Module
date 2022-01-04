8d0a4257776b47fa1665a3a31f210b0017d609f2
Alexdelia <alexandre.delille.57@gmail.com>
2022-01-04 14:26:45 +0100

    rename

--- CPP_Module_04/ex03/IMateriaSource.hpp
+++ CPP_Module_04/ex03/IMateriaSource.hpp
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 12:14:18 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/03 16:26:03 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/04 14:26:37 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATERIASOURCE_HPP
-# define MATERIASOURCE_HPP
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
 
 # include <iostream>
 # include <typeinfo>
 
-# include "AMateria"
+# include "AMateria.hpp"
 
-class MateriaSource
+class IMateriaSource
 {
 	public:
 		virtual ~IMateriaSource() {}
 		
 		virtual void		learnMateria(AMateria*) = 0;
-		virtual AMateria* 	createMateria(std::string const & type) = 0;
+		virtual AMateria* 	createMateria(const std::string &type) = 0;
 };
-
-//std::ostream &operator<<(std::ostream &o, MateriaSource const &src);
 
 #endif
