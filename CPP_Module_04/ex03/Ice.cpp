a83e13e03dc085ed1886611a8100854a8770d315
Alexdelia <alexandre.delille.57@gmail.com>
2022-01-04 17:28:29 +0100

    finish aditional test

--- CPP_Module_04/ex03/Ice.cpp
+++ CPP_Module_04/ex03/Ice.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 17:09:40 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/04 17:14:05 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/04 17:26:12 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ Ice	&Ice::operator=(const Ice &rhs)
 
 void	Ice::use(ICharacter &target)
 {
-	std::cout << "* shoots an ice bolt at" << target.getName() << " *" << std::endl;
+	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
 }
 
 AMateria	*Ice::clone(void) const
--- CPP_Module_04/ex03/main.cpp
+++ CPP_Module_04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:30:57 by adelille          #+#    #+#             */
-/*   Updated: 2022/01/04 14:14:52 by adelille         ###   ########.fr       */
+/*   Updated: 2022/01/04 17:27:43 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,72 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 
-int main()
+int	main(void)
 {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
+	{
+		std::cout << "subject test" << std::endl;
+	
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
-	ICharacter* me = new Character("me");
-	
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	
-	ICharacter* bob = new Character("bob");
-	
-	me->use(0, *bob);
-	me->use(1, *bob);
-	
-	delete bob;
-	delete me;
-	delete src;
-	
+		ICharacter* me = new Character("me");
+
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		ICharacter* bob = new Character("bob");
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+
+		delete bob;
+		delete me;
+		delete src;
+	}
+	{
+		std::cout << std::endl << "aditional test" << std::endl;
+		
+		IMateriaSource* src = new MateriaSource();
+		// more than 4 spell
+		src->learnMateria(new Ice());
+		src->learnMateria(new Ice());
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		src->learnMateria(new Cure());
+		src->learnMateria(new Cure());
+
+		ICharacter* me = new Character("me");
+
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		// unequip, delete and re-equip
+		me->unequip(1);
+		delete tmp;
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		ICharacter* bob = new Character("bob");
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+		me->use(2, *bob);
+		me->use(3, *bob);
+		me->use(4, *bob); // over index 3
+
+		bob->use(0, *me); // use bob's spell
+
+		delete bob;
+		delete me;
+		delete src;
+	}
+
 	return 0;
 }
