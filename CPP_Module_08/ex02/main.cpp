f91d3abd83864e8b1291de54836773b5874297c0
Alexdelia <alexandre.delille.57@gmail.com>
2022-01-17 13:33:13 +0100

    ex02

---
+++ CPP_Module_08/ex02/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/16 18:17:30 by adelille          #+#    #+#             */
+/*   Updated: 2022/01/17 13:32:33 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+
+int	main(void)
+{
+	{
+		MutantStack<int>	mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "top:\t" << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "size:\t" << mstack.size() << std::endl << std::endl;
+	}
+	{
+		MutantStack<int>	mstack;
+		
+		mstack.push(3);
+		mstack.push(42);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		std::cout << "stack is:" << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+
+	return (0);
+}
---
+++ CPP_Module_08/ex02/mutantstack.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/17 13:26:38 by adelille          #+#    #+#             */
+/*   Updated: 2022/01/17 13:27:48 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(void) : std::stack<T>() {};
+		MutantStack(const MutantStack &src)
+		{
+			*this = src;
+		}
+		~MutantStack() {};
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		MutantStack	&operator=(const MutantStack &rhs)
+		{
+			this->std::stack<T>::operator=(rhs);
+		}
+
+		iterator	begin(void)
+		{
+			return (this->c.begin());
+		}
+		iterator	end(void)
+		{
+			return (this->c.end());
+		}
+};
+
+#endif
